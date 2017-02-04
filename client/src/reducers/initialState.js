32b265bb5f25b363a4b2fe897a004f3263252f64
Majid <mhadaily@gmail.com>
2017-02-04 18:43:53 +0800

    Initial Stats

---
+++ client/src/reducers/initialState.js
@@ -0,0 +1,27 @@
+export default {
+  deceased: {
+    name: '',
+    surname: '',
+    address: {
+      postcode: null,
+      line1: '',
+      city: ''
+    },
+    dob: null,
+    martialStatus: '',
+  },
+  customer: {
+    name: '',
+    phone: '',
+    contactWay: '',
+    timeToCall: ''
+  },
+  funeral: {
+    type: ''
+  },
+  meta:{
+    dateCreated: null,
+    dateUpdated: null,
+    adminUser: null
+  }
+};
