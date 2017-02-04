8eab66c626ace7bafc056b1b5f9e08fd2a58cac4
Majid <mhadaily@gmail.com>
2017-02-04 18:46:01 +0800

    change links

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link, IndexLink } from 'react-router';
 import { AppBar, FontIcon } from 'react-toolbox';
+import theme from './header.scss';
 
 const MainHeader = () => {
   return (
-    <AppBar>
+    <AppBar theme={theme}>
+      {'MYMORIA'}
       <IndexLink to="/"><FontIcon value="home"/></IndexLink>
-      <Link to="/case/1"><FontIcon value="account_circle"/></Link>
-      <Link to="/About"><FontIcon value="info"/></Link>
+      <Link to="/case/"><FontIcon value="account_circle"/></Link>
+      <Link to="/notFound"><FontIcon value="info"/></Link>
     </AppBar>
   );
 };
