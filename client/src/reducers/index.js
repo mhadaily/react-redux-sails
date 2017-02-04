928fd45768e6d99babdbf5a1982e212e0c4ce4df
Majid <mhadaily@gmail.com>
2017-02-04 19:56:01 +0800

    Update RootReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-
+import caseCustomer from './caseSavingReducer';
 
 const rootReducer = combineReducers({
-  routing: routerReducer
+  routing: routerReducer,
+  caseCustomer
 });
 
 export default rootReducer;
