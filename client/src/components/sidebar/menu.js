83dbce7e58f8905a457fcca157013206e54d2322
Majid <mhadaily@gmail.com>
2017-02-04 17:55:51 +0800

    ad sidebar

---
+++ client/src/components/sidebar/menu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+import { List, ListItem, ListDivider } from 'react-toolbox';
+import style from "./menu.scss";
+
+const Menu = () => {
+  return (
+    <List ripple>
+      <Link className={style.noUnderline} to="/"><ListItem caption="Home" leftIcon="home"/></Link>
+      <Link className={style.noUnderline} to="/"><ListItem caption="Search" leftIcon="search"/></Link>
+      <ListDivider />
+      <Link className={style.noUnderline} to="/case"> <ListItem caption="Open A Case" leftIcon="send"/></Link>
+    </List>
+  );
+};
+
+export default Menu;
