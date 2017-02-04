0331d9fc4c5ff4118c5e8b643a3ddcc0627f16cd
Majid <mhadaily@gmail.com>
2017-02-04 19:55:31 +0800

    Add Saving Case Reducer + Test

---
+++ client/src/reducers/caseSavingReducer.js
@@ -0,0 +1,44 @@
+import initialState from './initialState';
+import objectAssign from 'object-assign';
+import { SAVE_DECEASED_DETAIL, SAVE_CUSTOMER_DETAIL, SAVE_FUNERAL } from '../constants/actionTypes';
+
+
+// IMPORTANT: Note that with Redux, state should NEVER be changed.
+// State is considered immutable. Instead,
+// create a copy of the state passed and set new values on the copy.
+// Note that I'm using Object.assign to create a copy of current state
+// and update values on the copy.
+
+function caseCustomer(state = initialState, action) {
+  let customer;
+  let deceased;
+  let funeral;
+
+  switch (action.type) {
+    case SAVE_FUNERAL:
+
+      funeral = objectAssign({}, state.funeral);
+      funeral[action.fieldName] = action.value;
+
+      return funeral;
+
+    case SAVE_DECEASED_DETAIL:
+
+      deceased = objectAssign({}, state.deceased);
+      deceased[action.fieldName] = action.value;
+
+      return deceased;
+
+
+    case SAVE_CUSTOMER_DETAIL:
+
+      customer = objectAssign({}, state.customer);
+      customer[action.fieldName] = action.value;
+
+      return customer;
+
+    default:
+      return state;
+  }
+}
+export default caseCustomer;
---
+++ client/src/reducers/caseSavingReducer.spec.js
@@ -0,0 +1,54 @@
+import { expect } from 'chai';
+import * as ActionTypes from '../constants/actionTypes';
+import reducer from './caseSavingReducer';
+import initialState from './initialState';
+import * as FuneralTypes from '../constants/FuneralTypes';
+import { describe, it } from 'mocha';
+
+describe('Reducers::caseSavingReducer', () => {
+
+  it('should set initial state by default', () => {
+    const action = {type: 'unknown'};
+    const expected = initialState;
+
+    expect(reducer(undefined, action)).to.deep.equal(expected);
+  });
+
+  it('should handle SAVE_FUNERAL', () => {
+    const action = {type: ActionTypes.SAVE_FUNERAL, settings: initialState, fieldName: 'type', value: FuneralTypes.BURIAL};
+
+    const expectedType = FuneralTypes.BURIAL;
+    const expectedSavings = {type: FuneralTypes.BURIAL};
+
+    expect(reducer(initialState, action).type).to.equal(expectedType);
+    expect(reducer(initialState, action)).to.deep.equal(expectedSavings);
+  });
+
+  it('should handle SAVE_CUSTOMER_DETAIL', () => {
+    const action = {type: ActionTypes.SAVE_CUSTOMER_DETAIL, settings: initialState, fieldName: 'name', value: 'Majid'};
+
+    const expectedName = 'Majid';
+    const expectedSavings = {name: 'Majid', phone: '', contactWay: '', timeToCall: ''};
+
+    expect(reducer(initialState, action).name).to.equal(expectedName);
+    expect(reducer(initialState, action)).to.deep.equal(expectedSavings);
+  });
+
+  it('should handle SAVE_DECEASED_DETAIL', () => {
+    const action = {type: ActionTypes.SAVE_DECEASED_DETAIL, settings: initialState, fieldName: 'surname', value: 'Hajian'};
+
+    const expectedSurname= 'Hajian';
+    const expectedSavings = { name: '',
+      surname: 'Hajian',
+      address: {
+        postcode: null,
+        line1: '',
+        city: ''
+      },
+      dob: null,
+      martialStatus: '',};
+
+    expect(reducer(initialState, action).surname).to.equal(expectedSurname);
+    expect(reducer(initialState, action)).to.deep.equal(expectedSavings);
+  });
+});
