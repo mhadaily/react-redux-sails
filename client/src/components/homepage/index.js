e1ab9bae498bbc121fe5d7a3d1dfc68e7ade46ac
Majid <mhadaily@gmail.com>
2017-02-06 01:01:44 +0800

    Show fake data for now

--- client/src/components/homepage/index.js
+++ client/src/components/homepage/index.js
@@ -1,58 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router';
-import Table from 'react-toolbox/lib/table';
-
-const UserModel = {
-  name: {type: String},
-  twitter: {type: String},
-  birthdate: {type: Date},
-  cats: {type: Number},
-  dogs: {type: Number},
-  active: {type: Boolean}
-};
-
-const users = [
-  {name: 'Javi Jimenez', twitter: '@soyjavi', birthdate: new Date(1980, 3, 11), cats: 1},
-  {name: 'Javi Velasco', twitter: '@javivelasco', birthdate: new Date(1987, 1, 1), dogs: 1, active: true},
-  {name: 'Javi Velasco', twitter: '@javivelasco', birthdate: new Date(1987, 1, 1), dogs: 1, active: true},
-  {name: 'Javi Velasco', twitter: '@javivelasco', birthdate: new Date(1987, 1, 1), dogs: 1, active: true},
-  {name: 'Javi Velasco', twitter: '@javivelasco', birthdate: new Date(1987, 1, 1), dogs: 1, active: true},
-  {name: 'Javi Velasco', twitter: '@javivelasco', birthdate: new Date(1987, 1, 1), dogs: 1, active: true},
-  {name: 'Javi Velasco', twitter: '@javivelasco', birthdate: new Date(1987, 1, 1), dogs: 1, active: true},
-  {name: 'Javi Velasco', twitter: '@javivelasco', birthdate: new Date(1987, 1, 1), dogs: 1, active: true},
-  {name: 'Javi Velasco', twitter: '@javivelasco', birthdate: new Date(1987, 1, 1), dogs: 1, active: true},
-];
-
-
+import { List, ListItem, ListSubHeader } from 'react-toolbox';
+import fakeData from '../../data/case';
 
 class HomePage extends React.Component {
 
   constructor(props) {
     super(props);
   }
-  state = { selected: [], source: users };
 
-  handleChange = (row, key, value) => {
-    const source = this.state.source;
-    source[row][key] = value;
-    this.setState({source});
-  };
-
-  handleSelect = (selected) => {
-    this.setState({selected});
-  };
-
-  render () {
+  render() {
     return (
-      <Table
-        model={UserModel}
-        onChange={this.handleChange}
-        onSelect={this.handleSelect}
-        selectable
-        multiSelectable
-        selected={this.state.selected}
-        source={this.state.source}
-      />
+      <List selectable ripple>
+        <ListSubHeader caption='Explore All Cases'/>
+        {fakeData.map(row => (
+          <Link to={'/case/' + row._id} key={row._id}>
+            <ListItem
+              avatar='https://dl.dropboxusercontent.com/u/2247264/assets/m.jpg'
+              caption={row.deceased.name + ' ' + row.deceased.sureName}
+              legend={row.deceased.dob + ' ' + row.deceased.martialStatus}
+              rightIcon='star'
+              leftIcon='send'
+            />
+          </Link>
+        ))}
+      </List>
     );
   }
 
