70fa57ef0ca8965e25c2b88e5c057a11e181aaf2
Majid <mhadaily@gmail.com>
2017-02-05 11:44:30 +0800

    Rename caseCustomer to Caseinfo in Store

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import caseCustomer from './caseSavingReducer';
+import caseInfo from './caseSavingReducer';
 
 const rootReducer = combineReducers({
   routing: routerReducer,
-  caseCustomer
+  caseInfo
 });
 
 export default rootReducer;
